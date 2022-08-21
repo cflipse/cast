8423c9e57d288b69c1e5acc4a77a17b280ee4e83
Chris Flipse <cflipse@digitalocean.com>
2022-08-21 00:55:14 -0400

    add verisoning table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_21_045003) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_21_045338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "hstore"
@@ -67,6 +67,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_21_045003) do
     t.index ["email"], name: "index_profiles_on_email"
     t.index ["login"], name: "index_profiles_on_login"
     t.index ["persistence_token"], name: "index_profiles_on_persistence_token"
+  end
+
+  create_table "versions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "episodes", "podcasts"
