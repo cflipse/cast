bb8e8edf0a444bae320d62d544512f657bd24eef
Chris Flipse <cflipse@digitalocean.com>
2022-08-21 00:37:34 -0400

    enable our core extensions

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_21_043500) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
+  enable_extension "hstore"
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
 end
