3cf8b6648aef59a962a14e8867fb55b42611c616
Chris Flipse <cflipse@gmail.com>
2022-09-05 10:29:50 -0400

    install and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "app/views/**/*",
+    "app/frontend/**/*.{jss,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
