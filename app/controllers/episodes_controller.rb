e5e92c945483a2ee559613f8c6960d3ec4511d7e
Chris Flipse <cflipse@gmail.com>
2022-09-05 12:30:27 -0400

    add spec for editing an episode

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,12 +3,20 @@ class EpisodesController < ApplicationController
     @podcast = Podcast.find_by(slug: params[:podcast_id])
   end
 
+  def show
+    @episode = @podcast.episodes.find(params[:id])
+  end
+
   def new
     @episode = @podcast.episodes.build(
       published: Date.today,
       number: (@podcast.episodes.maximum(:number) || 0).succ,
       explicit: @podcast.explicit
     )
+  end
+
+  def edit
+    @episode = @podcast.episodes.find(params[:id])
   end
 
   def create
@@ -20,6 +28,20 @@ class EpisodesController < ApplicationController
         notice: "#{@episode.title} has been added"
     else
       render :new
+    end
+  end
+
+  def update
+    @episode = @podcast.episodes.find(params[:id])
+
+    @episode.attributes = params.require(:episode)
+      .permit(:name, :number, :season, :published, :audio, :description, :show_notes, :explicit)
+
+    if @episode.save
+      redirect_to podcast_episode_path(@podcast, @episode),
+        notice: "#{@episode.title} has been updated"
+    else
+      render :edit
     end
   end
 end
--- spec/features/episode_spec.rb
+++ spec/features/episode_spec.rb
@@ -20,4 +20,25 @@ RSpec.feature "Episodes", type: :feature do
 
     expect(page).to have_content("Episode 4: Defilers are bad, mkay?")
   end
+
+  scenario "Edit an episode" do
+    episode = create :episode, podcast: podcast
+
+    visit podcast_path(podcast)
+    click_on episode.title
+
+    lorem = Faker::Lorem.paragraph(sentence_count: 5)
+
+    click_on "Edit Episode"
+
+    fill_in "Season", with: 2
+    fill_in "Number", with: 1
+
+    fill_in "Show notes", with: lorem
+
+    click_on "Update Episode"
+
+    expect(page).to have_content("Episode 1: #{episode.name}")
+      .and have_content(lorem)
+  end
 end
