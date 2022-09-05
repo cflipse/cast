15989b6af9aeee9ce026ce6238e6dfb6168cd7e0
Chris Flipse <cflipse@gmail.com>
2022-09-04 22:25:32 -0400

    add verification of form upload

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,6 +1,29 @@
 class EpisodesController < ApplicationController
-  def index
+  before_action do
     @podcast = Podcast.find(params[:podcast_id])
+  end
+
+  def index
     @episodes = @podcast.episodes
+  end
+
+  def new
+    @episode = @podcast.episodes.build(
+      published: Date.today,
+      number: (@podcast.episodes.maximum(:number) || 0).succ,
+      explicit: @podcast.explicit
+    )
+  end
+
+  def create
+    @episode = @podcast.episodes.build params.require(:episode)
+      .permit(:name, :number, :season, :published, :audio, :description, :show_notes, :explicit)
+
+    if @episode.save
+      redirect_to podcast_path(@podcast),
+        notice: "#{@episode.title} has been added"
+    else
+      render :new
+    end
   end
 end
--- spec/features/episode_spec.rb
+++ spec/features/episode_spec.rb
@@ -1,17 +1,23 @@
 require "rails_helper"
 
 RSpec.feature "Episodes", type: :feature do
-  scenario "Create a new episode" do
-    pending "check correctness"
+  let(:podcast) { create :podcast }
 
-    visit "/"
+  scenario "Create a new episode" do
+    visit podcast_path(podcast)
+
     click_on "New Episode"
 
-    fill_in "Title", with: "Defilers are bad, mkay?"
+    fill_in "Name", with: "Defilers are bad, mkay?"
+    fill_in "Number", with: 4
     fill_in "Published", with: "2021-10-30"
+
+    attach_file "Audio", Rails.root.join("spec/fixtures/bso001-templars.mp3")
 
     expect {
       click_on "Create Episode"
-    }.to change(Episode, count).by 1
+    }.to change(Episode, :count).by 1
+
+    expect(page).to have_content("Episode 4: Defilers are bad, mkay?")
   end
 end
