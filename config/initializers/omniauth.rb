51263d12589a53b7e5edf64fc456b79ce1db62ec
Chris Flipse <cflipse@gmail.com>
2022-09-25 11:09:39 -0400

    add configuration for omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
+  provider :google_oauth2, Rails.application.credentials.google_client_id,
+    Rails.application.credentials.google_client_secret
+end
