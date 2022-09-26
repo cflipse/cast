6c1571213d91da5a046aba929e1b5455f8016291
Chris Flipse <cflipse@gmail.com>
2022-09-26 10:35:29 -0400

    add login support

---
+++ app/controllers/authorizations_controller.rb
@@ -0,0 +1,16 @@
+class AuthorizationsController < ApplicationController
+  def create
+    email = request.env.dig("omniauth.auth", "info", "email")
+    profile = Profile.find_by(email: email)
+
+    if profile
+      session[:current_profile_id] = profile.id
+
+      redirect_to url_from(request.env["omniauth.origin"]) || root_path,
+        notice: "Welcome #{profile.display_name}",
+        fallback_url: root_path
+    else
+      redirect_to root_path, alert: "Unknown user #{email}"
+    end
+  end
+end
