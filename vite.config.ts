4dcb1cbc2ee0117f8d9a59f1297253c5c9672055
Chris Flipse <cflipse@gmail.com>
2022-09-29 17:43:46 -0400

    let vite run as https

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,16 @@ import RubyPlugin from 'vite-plugin-ruby'
 
 export default defineConfig({
   plugins: [
+    RubyPlugin(),
     StimulusHMR(),
     FullReload(['config/routes.rb', 'app/views/**/*', 'app/frontend/**/*', 'app/builders/**/*', 'app/components/**/*', 'app/helpers/**/*']),
-    RubyPlugin(),
   ],
+  server: {
+    "port": 3046,
+    "https": true,
+    "hmr": {
+      "protocol": "wss",
+      "host": "dev.athas.org",
+    }
+  }
 })
