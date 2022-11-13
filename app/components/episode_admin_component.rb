494421943c7d43e680f3254ce46a903c2dfeb56a
Chris Flipse <cflipse@gmail.com>
2022-11-13 13:20:47 -0500

    extract episode admin controls to a compnent

---
+++ app/components/episode_admin_component.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class EpisodeAdminComponent < ApplicationComponent
+  extend Dry::Initializer
+
+  param :podcast
+  param :episode
+
+  option :policy
+
+  delegate :edit?, :destroy?, to: :policy
+
+  def render?
+    edit? || detroy?
+  end
+
+  def delete_classes
+    rounding = edit? ? "rounded-r-md" : "rounded-md"
+    "bg-red-700 text-gray-300 p-2 #{rounding}"
+  end
+
+  def delete_form_attributes
+    {
+      class: "inline-block",
+      data: {"turbo-confirm": "Are you sure you want to remove this episode?"}
+    }
+  end
+
+  def edit_classes
+    rounding = destroy? ? "rounded-l-md" : "rounded-md"
+    "bg-amber-700 text-gray-300 p-2 #{rounding}"
+  end
+end
