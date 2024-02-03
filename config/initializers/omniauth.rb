7920d4dc608e9c7f66e2b64f554252c634f97260
Chris Flipse <cflipse@gmail.com>
2024-02-03 11:34:30 -0500

    make redirect URI an environment variable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :google_oauth2, Rails.application.credentials.google_client_id,
     Rails.application.credentials.google_client_secret,
-    redirect_uri: "https://cast.athas.org/auth/google_oauth2/callback"
-
+    redirect_uri: ENV["OMNIAUTH_REDIRECT_URI"].presence
 end
