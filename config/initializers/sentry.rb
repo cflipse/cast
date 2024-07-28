14b80919e3b55bcea79e7024eececc4673d56c4b
Chris Flipse <cflipse@gmail.com>
2024-07-28 13:39:51 -0400

    update sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -10,6 +10,19 @@ if Rails.application.credentials.sentry_dsn
     # We recommend adjusting this value in production
     config.traces_sample_rate = 0.0005
 
+    config.traces_sampler = ->(context) do
+      next context[:parent_sampled] unless context[:parent_sampled].nil?
+
+      rack_env = context[:env]
+
+      # skip tracing for health check endpoints
+      return false if rack_env["PATH_INFO"] =~ %r{^/up/}i
+
+      0.005
+    end
+
+    config.release = ENV["KAMAL_VERSION"].presence
+
     config.excluded_exceptions += %w[
       ActionController::RoutingError
       ActiveRecord::RecordNotFound
