cc50edc6f51b10aa573ef41a15ee158237b5d3a2
Chris Flipse <cflipse@gmail.com>
2025-12-25 00:01:53 -0500

    add migration for existing database structure

---
+++ hanami/config/db/migrate/20251224224626_create_existing.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+ROM::SQL.migration do
+  # Add your migration here.
+  #
+  # See https://guides.hanamirb.org/v2.2/database/migrations/ for details.
+  change do
+    create_table :podcasts do
+      column :id, "varchar(36)", null: false, primary_key: true, unique: true
+      column :name, :text, null: false, collate: :nocase
+      column :slug, :text, null: false, collate: :nocase, index: true
+      column :description, :text
+      column :explicit, :boolean, null: false, default: false
+      column :image_data, :jsonb
+      column :tagline, :text
+
+      column :created_at, :datetime, null: false
+      column :updated_at, :datetime, null: false
+    end
+
+    create_table :profiles do
+      column :id, "varchar(36)", null: false, primary_key: true, unique: true
+      column :login, :text, null: false, collate: :nocase, index: true
+      column :email, :text, null: false, collate: :nocase, index: true
+
+      column :persistence_token, String, index: true
+
+      column :display_name, String, null: false
+      column :bio, :text
+      column :avatar, :text
+
+      column :roles, :jsonb, null: false, default: "[]"
+
+      column :created_at, :datetime, null: false
+      column :updated_at, :datetime, null: false
+      column :admin, :boolean, null: false, default: false
+    end
+
+    create_table :podcast_hosts do
+      column :id, "varchar(36)", null: false, primary_key: true, unique: true
+      foreign_key :profile_id, :profiles, type: "varchar(36)", null: false, index: true
+      foreign_key :podcast_id, :podcasts, type: "varchar(36)", null: false, index: true
+
+      column :state, String, collate: :nocase
+      column :created_at, :datetime, null: false
+      column :updated_at, :datetime, null: false
+    end
+
+    create_table :episodes do
+      column :id, "varchar(36)", null: false, primary_key: true, unique: true
+      foreign_key :podcast_id, :podcasts, type: "varchar(36)", null: false, index: true
+
+      column :name, String, null: false, collate: :nocase
+      column :number, Integer
+      column :season, Integer
+      column :description, :text
+      column :audio_data, :jsonb
+      column :show_notes, :text
+      column :explicit, :boolean, null: false, default: false
+
+      column :published, :datetime, null: true, index: true
+      column :created_at, :datetime, null: false
+      column :updated_at, :datetime, null: false
+      column :deleted_at, :datetime, null: true
+
+      # NOTE: elements in here can't be directly indexed in sqlite; figure out
+      # the function, or denormalize these.  Included like this for parity with the
+      # existing db structure
+      column :slugs, :jsonb, default: "[]", null: false
+    end
+  end
+end
---
+++ hanami/config/db/structure.sql
@@ -0,0 +1,60 @@
+CREATE TABLE `schema_migrations`(`filename` varchar(255) NOT NULL PRIMARY KEY);
+CREATE TABLE `podcasts`(
+  `id` varchar(36) NOT NULL UNIQUE PRIMARY KEY,
+  `name` text COLLATE nocase NOT NULL,
+  `slug` text COLLATE nocase NOT NULL,
+  `description` text,
+  `explicit` boolean DEFAULT(0) NOT NULL,
+  `image_data` jsonb,
+  `tagline` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+);
+CREATE INDEX `podcasts_slug_index` ON `podcasts`(`slug`);
+CREATE TABLE `profiles`(
+  `id` varchar(36) NOT NULL UNIQUE PRIMARY KEY,
+  `login` text COLLATE nocase NOT NULL,
+  `email` text COLLATE nocase NOT NULL,
+  `persistence_token` varchar(255),
+  `display_name` varchar(255) NOT NULL,
+  `bio` text,
+  `avatar` text,
+  `roles` jsonb DEFAULT('[]') NOT NULL, `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `admin` boolean DEFAULT(0) NOT NULL
+);
+CREATE INDEX `profiles_login_index` ON `profiles`(`login`);
+CREATE INDEX `profiles_email_index` ON `profiles`(`email`);
+CREATE INDEX `profiles_persistence_token_index` ON `profiles`(
+  `persistence_token`
+);
+CREATE TABLE `podcast_hosts`(
+  `id` varchar(36) NOT NULL UNIQUE PRIMARY KEY,
+  `profile_id` varchar(36) NOT NULL REFERENCES `profiles`,
+  `podcast_id` varchar(36) NOT NULL REFERENCES `podcasts`,
+  `state` varchar(255) COLLATE nocase,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+);
+CREATE INDEX `podcast_hosts_profile_id_index` ON `podcast_hosts`(`profile_id`);
+CREATE INDEX `podcast_hosts_podcast_id_index` ON `podcast_hosts`(`podcast_id`);
+CREATE TABLE `episodes`(
+  `id` varchar(36) NOT NULL UNIQUE PRIMARY KEY,
+  `podcast_id` varchar(36) NOT NULL REFERENCES `podcasts`,
+  `name` varchar(255) COLLATE nocase NOT NULL,
+  `number` integer,
+  `season` integer,
+  `description` text,
+  `audio_data` jsonb,
+  `show_notes` text,
+  `explicit` boolean DEFAULT(0) NOT NULL,
+  `published` datetime NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime NULL,
+  `slugs` jsonb DEFAULT('[]') NOT NULL
+);
+CREATE INDEX `episodes_podcast_id_index` ON `episodes`(`podcast_id`);
+CREATE INDEX `episodes_published_index` ON `episodes`(`published`);
+INSERT INTO schema_migrations (filename) VALUES
+('20251224224626_create_existing.rb');
