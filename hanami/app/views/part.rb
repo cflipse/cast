708da24c37cda88de63ade09e3253585d0b382bf
Chris Flipse <cflipse@gmail.com>
2026-07-16 01:35:14 -0400

    drop existing hanami
    just going to  start this over, regen from a fresh 3.0 install

--- hanami/app/views/part.rb
+++
@@ -1,9 +0,0 @@
-# auto_register: false
-# frozen_string_literal: true
-
-module Casts
-  module Views
-    class Part < Hanami::View::Part
-    end
-  end
-end
--- hanami/spec/support/factory.rb
+++
@@ -1,8 +0,0 @@
-require "rom/factory"
-require "faker"
-
-Factory = ROM::Factory.configure do |config|
-  config.rom = Casts::App.container["db.rom"]
-end
-
-Dir["#{File.dirname(__FILE__)}/../factories/*.rb"].each { |file| require file }
