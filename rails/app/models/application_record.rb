94ef813f4f800be7a1a077749ab648ae860ade31
Chris Flipse <cflipse@gmail.com>
2026-07-27 14:52:12 -0400

    use sqlite in test

--- rails/app/models/application_record.rb
+++ rails/app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  if Rails.env.test?
+    connects_to database: {writing: :sqlite, reading: :sqlite}
+  end
+
+  before_create -> { self.id ||= SecureRandom.uuid }
 end
