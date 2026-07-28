9f67cb51bbff3d5d7049eaf8c29ba77d9315330a
Chris Flipse <cflipse@gmail.com>
2026-07-28 16:16:46 -0400

    generate a homepage action

---
+++ hanami/app/views/home/index.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Cast
+  module Views
+    module Home
+      class Index < Cast::View
+      end
+    end
+  end
+end
--- hanami/config/routes.rb
+++ hanami/config/routes.rb
@@ -3,5 +3,6 @@
 module Cast
   class Routes < Hanami::Routes
     # Add your routes here. See https://hanakai.org/learn/hanami/routing/ for details.
+    get "/home", to: "home.index"
   end
 end
