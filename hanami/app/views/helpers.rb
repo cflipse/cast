8104478dafab55cd8e43099ddff003d82accd7c3
Chris Flipse <cflipse@gmail.com>
2026-09-14 17:08:56 -0400

    configure and set up vite

--- hanami/app/views/helpers.rb
+++ hanami/app/views/helpers.rb
@@ -1,10 +1,35 @@
 # auto_register: false
 # frozen_string_literal: true
 
+require "vite_ruby"
+
 module Cast
   module Views
     module Helpers
       # Add your view helpers here
+
+      def vite_client_tag
+        return unless ViteRuby.instance.dev_server_running?
+
+        config = ViteRuby.instance.config
+
+        src = "#{config.protocol}://#{config.host_with_port}/#{config.public_output_dir}/@vite/client"
+        tag.script(type: "module", src: src)
+      end
+
+      def vite_javascript_tag(name)
+        src = ViteRuby.instance.manifest.path_for(name, type: :javascript)
+        tag.script(src: src, type: "module")
+      end
+
+      def vite_stylesheet_tag(name)
+        src = ViteRuby.instance.manifest.path_for(name, type: :stylesheet)
+        tag.link(rel: "stylesheet", href: src)
+      end
+
+      def vite_asset_path(name)
+        ViteRuby.instance.manifest.path_for(name)
+      end
     end
   end
 end
--- hanami/config/app.rb
+++ hanami/config/app.rb
@@ -5,8 +5,13 @@ require "hanami"
 module Cast
   class App < Hanami::App
     environment(:development) do
+      require "vite_ruby" 
+
+      config.middleware.use ViteRuby::DevServerProxy
+
       config.actions.content_security_policy[:img_src] += " http://localhost:*"
       config.actions.content_security_policy[:media_src] += " http://localhost:*"
+      config.actions.content_security_policy[:script_src] += " http://localhost:*"
     end
   end
 end
